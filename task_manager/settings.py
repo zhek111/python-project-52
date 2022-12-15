8b65fdefab0aa09d996026b23a79aadf4c88efd8
evgeny <zhek12322@gmail.com>
2022-12-15 11:51:13 +0300

    init16

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -130,4 +130,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'task_manager/static')
