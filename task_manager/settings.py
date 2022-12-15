818a31df748532a08cdee981b62cbb13ab9949a7
evgeny <zhek12322@gmail.com>
2022-12-15 09:47:50 +0300

    init8

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -130,4 +130,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+
 from task_manager import views
 
 urlpatterns = [
     path('', views.IndexView.as_view()),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
