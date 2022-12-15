2a9c72c37f6ed6ca6227311d52dfa1bd632ba038
evgeny <zhek12322@gmail.com>
2022-12-15 08:56:00 +0300

    init6

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', True)
 
-ALLOWED_HOSTS = ['webserver',
+ALLOWED_HOSTS = ['webserver', 
                  '127.0.0.1', 'web-production-8bc6.up.railway.app']
 
 
@@ -130,5 +130,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
